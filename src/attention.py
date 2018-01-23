47161808e774e5cdcdffe3ccbade1dfe86b4e83c
John J. Kim <jskim@selvas.com>
2018-01-23 09:35:16 +0900

    JARVIS-705 오류 수정 및 코드 정리

--- src/attention.py
+++ src/attention.py
@@ -15,7 +15,7 @@ linear = rnn_cell_impl._linear
 
 def attention_decoder(encoder_outputs_raw,
                       encoder_state,
-                      num_decoder_symbols,
+                      decoder_output_size,
                       timestep_size,
                       num_heads=1,
                       dtype=tf.float32,
@@ -25,8 +25,8 @@ def attention_decoder(encoder_outputs_raw,
 
     with tf.variable_scope(scope or "attention_RNN"):
         encoder_outputs = array_ops.transpose(encoder_outputs_raw, [1, 0, 2])
-        output_size = encoder_outputs.get_shape()[2].value
-        top_states = [tf.reshape(encoder_outputs[ti], [-1, 1, output_size])
+        encoder_output_size = encoder_outputs.get_shape()[2].value
+        top_states = [tf.reshape(encoder_outputs[ti], [-1, 1, encoder_output_size])
                       for ti in range(timestep_size)]
         attention_states = tf.concat(top_states, 1)
         if not attention_states.get_shape()[1:2].is_fully_defined():
@@ -34,29 +34,29 @@ def attention_decoder(encoder_outputs_raw,
                              % attention_states.get_shape())
 
         batch_size = tf.shape(top_states[0])[0]  # Needed for reshaping.
-        attn_length = attention_states.get_shape()[1].value
-        attn_size = attention_states.get_shape()[2].value
+        if attention_states.get_shape()[1].value != timestep_size:
+            raise ValueError("timestep size must be constant")
+        attn_size = encoder_output_size
 
         # To calculate W1 * h_t we use a 1-by-1 convolution, need to reshape before.
         hidden = tf.reshape(
-            attention_states, [-1, attn_length, 1, attn_size])
+            attention_states, [-1, timestep_size, 1, attn_size])
         hidden_features = []
         v = []
-        attention_vec_size = attn_size  # Size of query vectors for attention.
         for a in range(num_heads):
             k = tf.get_variable("AttnW_%d" % a,
-                                [1, 1, attn_size, attention_vec_size])
+                                [1, 1, attn_size, decoder_output_size])
             hidden_features.append(tf.nn.conv2d(hidden, k, [1, 1, 1, 1], "SAME"))
             v.append(tf.get_variable("AttnV_%d" % a,
-                                     [attention_vec_size]))
+                                     [decoder_output_size]))
 
         def attention(query):
             attn_weights = []
             ds = []  # Results of attention reads will be stored here.
             for i in range(num_heads):
                 with tf.variable_scope("Attention_%d" % i):
-                    y = linear(query, attention_vec_size, True)
-                    y = tf.reshape(y, [-1, 1, 1, attention_vec_size])
+                    y = linear(query, decoder_output_size, True)
+                    y = tf.reshape(y, [-1, 1, 1, decoder_output_size])
                     # Attention mask is a softmax of v^T * tanh(...).
                     s = tf.reduce_sum(
                         v[i] * tf.tanh(hidden_features[i] + y), [2, 3])
@@ -64,7 +64,7 @@ def attention_decoder(encoder_outputs_raw,
                     attn_weights.append(a)
                     # Now calculate the attention-weighted vector d.
                     d = tf.reduce_sum(
-                        tf.reshape(a, [-1, attn_length, 1, 1]) * hidden,
+                        tf.reshape(a, [-1, timestep_size, 1, 1]) * hidden,
                         [1, 2])
                     ds.append(tf.reshape(d, [-1, attn_size]))
             return attn_weights, ds
@@ -79,18 +79,17 @@ def attention_decoder(encoder_outputs_raw,
         attention_encoder_outputs = list()
         sequence_attention_weights = list()
         for i in range(timestep_size):
-            if i > 0:
-                tf.get_variable_scope().reuse_variables()
             if i == 0:
                 with tf.variable_scope("Initial_Decoder_Attention"):
-                    initial_state = linear(encoder_state, output_size, True)
+                    initial_state = linear(encoder_state, decoder_output_size, True)
                 attn_weights, ds = attention(initial_state)
             else:
+                tf.get_variable_scope().reuse_variables()
                 attn_weights, ds = attention(logit)
             output = tf.concat([ds[0], encoder_outputs[i]], 1)
             # NOTE: here we temporarily assume num_head = 1
             with tf.variable_scope("AttnRnnOutputProjection"):
-                logit = linear(output, num_decoder_symbols, True)
+                logit = linear(output, decoder_output_size, True)
             attention_encoder_outputs.append(tf.expand_dims(logit, 1))
             # NOTE: here we temporarily assume num_head = 1
             sequence_attention_weights.append(attn_weights[0])
